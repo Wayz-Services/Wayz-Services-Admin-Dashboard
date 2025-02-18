7c7564aee4c83abcbd0be2d06d1ddcb17c4b4047
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2025-02-18 08:18:20 +0200

    improve the LocalizedLink component

--- app/components/LocalizedLink.tsx
+++ app/components/LocalizedLink.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { useLocale } from "next-intl";
 import Link, { LinkProps } from "next/link";
-import { ComponentProps } from "react";
+import { useLocale } from "next-intl";
+import { ComponentProps, useMemo } from "react";
 
 type LocalizedLinkProps = LinkProps &
   ComponentProps<"a"> & {
     children: React.ReactNode;
-    hreflang?: string; // Optional if you want to customize
+    hreflang?: string;
   };
 
 export default function LocalizedLink({
@@ -18,16 +18,19 @@ export default function LocalizedLink({
 }: LocalizedLinkProps) {
   const locale = useLocale();
 
-  // Prepend locale if href is not already localized
-  const localizedHref =
-    typeof href === "string" && !href.startsWith(`/${locale}`)
-      ? `/${locale}${href}`
-      : href;
+  // Memoize the localized href to avoid unnecessary recalculations
+  const localizedHref = useMemo(() => {
+    if (typeof href === "string") {
+      // If href is a string, prepend the locale if it's not already included
+      return href.startsWith(`/${locale}`) ? href : `/${locale}${href}`;
+    }
+    return href;
+  }, [href, locale]);
 
   return (
     <Link
       href={localizedHref}
-      hrefLang={hreflang || locale} // THIS WORKS DIRECTLY ON LINK NOW!
+      hrefLang={hreflang || locale} // Use the provided hreflang or fallback to the current locale
       {...props}
     >
       {children}
