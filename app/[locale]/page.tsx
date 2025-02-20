a759f94353deb7156c63febac48e82f805f3f266
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2025-02-20 12:57:54 +0200

    header as a UI is done

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,45 @@
-import { useTranslations } from "next-intl";
-import LocalizedLink from "../components/LocalizedLink";
+"use client";
+
+import Image from "next/image";
+import ThemeSwitcher from "../utils/ThemeSwitcher";
 
 export default function Home() {
-  const t = useTranslations("HomePage");
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-white dark:bg-black">
-      <h1>{t("title")}</h1>
-      <LocalizedLink href={"/about"}>{t("about")}</LocalizedLink>
-    </div>
+    <nav className="bg-[#0C8CE9] flex pt-10 items-start justify-around mx-auto fill-white dark:bg-black">
+      <Image
+        src={"/logo.png"}
+        width={150}
+        height={70}
+        className="relative"
+        alt="wayz logo"
+      />
+
+      <div className="flex items-center">
+        <select className="bg-transparent pe-1 text-white outline-none">
+          <option className="text-black">EN</option>
+          <option className="text-black">AR</option>
+        </select>
+
+        <div className="h-8 w-[1px] mx-3 border-e py-1 border-e-white me-3 dark:border-e-white" />
+
+        <ThemeSwitcher />
+      </div>
+
+      <ul className="flex items-center gap-7 font-bold text-white">
+        <li>Our services</li>
+        <li>I'm a Supplier</li>
+        <li>More About Wayz</li>
+      </ul>
+
+      <div />
+      <div />
+
+      <div className="flex items-center text-white font-semibold gap-5">
+        <button>Sign in</button>
+        <button className="bg-white px-4 rounded-lg py-2 text-black">
+          Post your task
+        </button>
+      </div>
+    </nav>
   );
 }
