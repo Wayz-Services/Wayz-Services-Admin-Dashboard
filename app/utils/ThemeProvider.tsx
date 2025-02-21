014438321cee0fe0398c823ff6443d35aab57937
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2025-02-21 12:25:12 +0200

    separate the header in a different file, add font feature to the app using next/font/google

--- app/utils/ThemeProvider.tsx
+++ app/utils/ThemeProvider.tsx
@@ -1,6 +1,10 @@
 "use client";
+
 import { useEffect, useState } from "react";
-import { ThemeProvider as NextThemesProvider, ThemeProviderProps as NextThemeProviderProps } from "next-themes";
+import {
+  ThemeProvider as NextThemesProvider,
+  ThemeProviderProps as NextThemeProviderProps,
+} from "next-themes";
 import { ReactNode } from "react";
 
 // Define props for your custom ThemeProvider component
@@ -8,7 +12,10 @@ interface ThemeProviderProps extends NextThemeProviderProps {
   children: ReactNode;
 }
 
-export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export default function ThemeProvider({
+  children,
+  ...props
+}: ThemeProviderProps) {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
