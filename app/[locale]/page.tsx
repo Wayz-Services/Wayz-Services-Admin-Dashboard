e01cb2408869420267905bf95f1a5136ccc56f38
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2025-02-24 09:04:34 +0200

    fix the miss curly braces for the page.tsx file in the [locale] folder

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { useTranslations } from "next-intl";
-import LocalizedLink from "../components/LocalizedLink";
 import Carousel from "../components/Carousel";
-import Card from "../components/Card";
 
 export default function Home() {
   const t = useTranslations("HomePage");
@@ -13,3 +11,4 @@ export default function Home() {
       <Carousel />
     </div>
   );
+}
