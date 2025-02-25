44da7943150a537402778aeec9f1560c609ad46a
Hani-kanaan <hani.kanaan85@yahoo.com>
2025-02-25 12:40:10 +0200

    search bar

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,14 +3,13 @@
 import { useTranslations } from "next-intl";
 import Carousel from "../components/Carousel";
 import YourProcess from "../components/YourProcess";
-
+import SearchBar from "../components/SearchBar";
 export default function Home() {
   const t = useTranslations("HomePage");
 
   return (
     <div>
-      <YourProcess />
-      <Carousel />
+      <SearchBar/>
     </div>
   );
 }
