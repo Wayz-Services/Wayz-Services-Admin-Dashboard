2ea13bcf57955f0669c3ea6ace22ce088323570b
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2025-02-26 09:50:09 +0200

    add backgColor prop to the Button

--- app/components/Reusables/Button.tsx
+++ app/components/Reusables/Button.tsx
@@ -4,7 +4,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   redirectTo?: string;
   onSubmit?: () => void;
-  backgColor: string;
+  backgColor?: string;
 }
 
 const Button: React.FC<ButtonProps> = ({
