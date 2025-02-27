264d80325ab8f73d88c233dcd82dbc18da841963
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2025-02-27 09:00:22 +0200

    add the background waves to the services section

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -12,26 +12,32 @@ const Services: React.FC = () => {
       title: t("vehiclesServicesTitle"),
       description: t("vehiclesServicesDescription"),
       altText: t("vehiclesServicesAlt"),
-      imageSrc: "/Rectangle.png",
+      imageSrc: "/Images/Vehicles.png",
     },
     {
       title: t("buildingServicesTitle"),
       description: t("buildingServicesDescription"),
       altText: t("buildingServicesAlt"),
-      imageSrc: "/Rectangle2.png",
+      imageSrc: "/Images/Buildings.png",
     },
   ];
 
   return (
-    <div className="flex flex-col items-center bg-primary p-5">
-      <div className="flex flex-col ">
-        <div className="text-white text-3xl">Our Services</div>
-        <div className="text-white text-4xl mt-2 mb-4 font-semibold">Check WAYZ Services</div>
+    <div
+      className="relative flex flex-col items-center bg-primary px-4 py-14 lg:py-24 overflow-hidden bg-[url('/Images/Element.png')]  bg-no-repeat bg-bottom"
+      style={{ backgroundSize: "100% 50%" }}
+    >
+      <div className="flex flex-col items-center">
+        <div className="text-white text-xl md:text-3xl">{t("ourServices")}</div>
 
-        <div className="flex flex-wrap gap-[40px] mt-4">
+        <div className="text-white text-2xl md:text-4xl mt-2 mb-4 font-semibold">
+          {t("checkWayzServices")}
+        </div>
+
+        <div className="flex items-center justify-center flex-wrap gap-[40px] mt-4">
           {services.map((service, index) => (
             <div key={index} className="rounded-xl overflow-hidden relative">
-              <div className="relative overflow-hidden w-[350px] h-[350px] sm:w-[400px] sm:h-[400px] md:w-[430px] md:h-[430px] lg:w-[460px] lg:h-[460px] xl:w-[600px] xl:h-[600px]">
+              <div className="relative overflow-hidden w-[330px] h-[330px] sm:w-[400px] sm:h-[400px] md:w-[430px] md:h-[430px] lg:w-[460px] lg:h-[460px] xl:w-[600px] xl:h-[600px]">
                 <Image
                   src={service.imageSrc}
                   alt={service.altText}
@@ -39,7 +45,7 @@ const Services: React.FC = () => {
                   unoptimized
                 />
 
-                <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,0.1)] via-[rgba(0,0,0,0.5)]" />
+                <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,0.1)] via-[rgba(0,0,0,0.1)]" />
               </div>
               <div className="relative">
                 <div className="absolute bottom-[60px] left-[20px] w-[calc(100%-40px)] text-white p-4">
@@ -51,7 +57,7 @@ const Services: React.FC = () => {
 
                 <div className="left-[22px] w-[calc(100%-40px)] h-[calc(100%-100px)] bg-black opacity-30 z-0" />
               </div>
-              <div className="absolute top-4 right-4 text-white">
+              <div className="absolute top-4 ltr:right-4 rtl:left-4 rtl:rotate-[-90deg] text-white">
                 <MdOutlineArrowOutward size={90} />
               </div>
             </div>
