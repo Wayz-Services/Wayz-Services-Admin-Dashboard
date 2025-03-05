ae628639c55161df390d81a4d997130f1c781433
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2025-03-05 14:13:17 +0200

    fix the design for the signIn

--- app/[locale]/(Auth)/signIn/page.tsx
+++ app/[locale]/(Auth)/signIn/page.tsx
@@ -1,12 +1,14 @@
-"use client"
+"use client";
+
 import React, { useState } from "react";
 import SignWrapper from "@/app/components/SignWrapper";
 import { FcGoogle } from "react-icons/fc";
 import Input from "@/app/components/Reusables/Input";
 import Button from "@/app/components/Reusables/Button";
 import LocalizedLink from "@/app/components/Reusables/LocalizedLink";
+
 const HorizontalSeparator = () => {
-  return <div className="h-[1px]  flex-grow  bg-[#377FC1] hidden md:block" />;
+  return <div className="h-[1px] w-full bg-[#377FC1] hidden md:block" />;
 };
 
 const Signin = () => {
@@ -17,49 +19,60 @@ const Signin = () => {
   return (
     <SignWrapper>
       {/* Adjust container width on larger screens */}
-      <div className="flex flex-col items-start   md:w-3/4 lg:w-1/2 ">
+      <div className="flex flex-col items-start md:w-3/4 lg:w-1/2 ">
         <h1 className="mb-4 text-xl sm:text-2xl text-center text-white">
           Welcome back
         </h1>
+
         <h2 className="text-3xl sm:text-5xl font-semibold mb-6 text-center">
           Sign In Now
         </h2>
 
-        <div className="w-full max-w-md">
-          <form className="w-full">
+        <div className="w-full">
+          <form>
             <div className="mb-4">
               <Input
+                name="Email"
                 label="Email Address"
                 value={email}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} 
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setEmail(e.target.value)
+                }
               />
             </div>
+
             <div className="mb-6">
               <Input
                 label="Password"
+                name="password"
                 value={password}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPassword(e.target.value)
+                }
                 isPassword
                 showPassword={showPassword}
                 setShowPassword={setShowPassword}
               />
             </div>
-            <div className="flex flex-col sm:flex-row items-center justify-between mb-4">
+
+            <div className="flex items-center justify-between mb-4">
               <label className="flex items-center">
                 <input
                   type="checkbox"
-                  className="form-checkbox accent-[#1F6EB6]"
-                  style={{ accentColor: "#1F6EB6" }}
+                  className="accent-[#fff] border border-[#64A6E3] rounded-md"
                 />
+
                 <span className="ml-2 text-sm text-white">Remember Me</span>
               </label>
+
               <LocalizedLink
                 className="inline-block  text-sm text-[#F6B60B] hover:text-blue-800 mt-2 sm:mt-0"
-                href="#" 
+                href="#"
               >
                 Forgot Password?
               </LocalizedLink>
             </div>
+
             <Button
               className="font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
               backgColor="white"
@@ -67,7 +80,6 @@ const Signin = () => {
             >
               Sign In
             </Button>
-
           </form>
 
           {/* Centered Section Below the Form */}
@@ -75,25 +87,25 @@ const Signin = () => {
             <p className="text-white text-center">
               Don't Have An Account Yet?{" "}
               <LocalizedLink
-                href="#"  
+                href="/signUp"
                 className="text-[#F6B60B] font-semibold hover:underline"
               >
                 Sign Up
               </LocalizedLink>
             </p>
+          </div>
 
-            <div className="mt-6">
-              <div className="flex items-center my-4">
-                <HorizontalSeparator />
-                <span className="mx-4 text-white">Or</span>
-                <HorizontalSeparator />
-              </div>
+          <div className="flex items-center my-4">
+            <HorizontalSeparator />
+            <span className="mx-4 text-white">Or</span>
+            <HorizontalSeparator />
+          </div>
 
-              <button className="flex items-center justify-center border rounded-full py-2 px-4 mt-4 bg-[#0D4170] border-none">
-                <FcGoogle className="me-2" size={22} />
-                Continue With Google
-              </button>
-            </div>
+          <div className="flex items-center justify-center">
+            <button className="flex items-center justify-center border rounded-full py-2 px-4 bg-[#0D4170] border-none hover:opacity-70">
+              <FcGoogle className="me-2" size={22} />
+              Continue With Google
+            </button>
           </div>
         </div>
       </div>
