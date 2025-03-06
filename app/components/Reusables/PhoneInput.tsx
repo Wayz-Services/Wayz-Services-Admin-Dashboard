4a628f9dd24848f97cb40122a4a2e16a05e342e4
Mahmoudkhoder01 <mahmoudkhodor06@gmail.com>
2025-03-06 09:17:41 +0200

    fix the phone input for arabic version

--- app/components/Reusables/PhoneInput.tsx
+++ app/components/Reusables/PhoneInput.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import ReactPhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 
@@ -6,43 +7,46 @@ interface PhoneInputProps {
 }
 
 const PhoneInput: React.FC<PhoneInputProps> = ({ setPhoneNumber }) => {
+  const t = useTranslations("SignUp");
+
   const handlePhoneChange = (value: any, country: any) => {
     setPhoneNumber(country.dialCode);
-    console.log(country.dialCode);
   };
 
   return (
     <div>
-      <label className="font-medium text-white">Phone Number</label>
+      <label className="font-medium text-white">{t("phoneNumber")}</label>
 
-      <ReactPhoneInput
-        country={"lb"}
-        onChange={handlePhoneChange}
-        enableSearch
-        containerStyle={{
-          color: "black",
-          height: "42px",
-          margin: "4px 0",
-        }}
-        inputStyle={{
-          fontSize: "16px",
-          fontFamily: "Inter",
-          width: "100%",
-          height: "42px",
-          borderRadius: "6px",
-          border: "1px solid #76A4CE",
-          paddingLeft: "50px",
-          color: "white",
-          backgroundColor: "#1F6EB6",
-        }}
-        buttonStyle={{
-          borderTopLeftRadius: "6px",
-          borderBottomLeftRadius: "6px",
-          background: "#1F6EB6",
-          border: "1px solid #76A4CE",
-          fontSize: "16px",
-        }}
-      />
+      <div style={{ direction: "ltr" }}>
+        <ReactPhoneInput
+          country={"lb"}
+          onChange={handlePhoneChange}
+          enableSearch
+          containerStyle={{
+            color: "black",
+            height: "42px",
+            margin: "4px 0",
+          }}
+          inputStyle={{
+            fontSize: "16px",
+            fontFamily: "Inter",
+            width: "100%",
+            height: "42px",
+            borderRadius: "6px",
+            border: "1px solid #76A4CE",
+            paddingLeft: "50px",
+            color: "white",
+            backgroundColor: "#1F6EB6",
+          }}
+          buttonStyle={{
+            borderTopLeftRadius: "6px",
+            borderBottomLeftRadius: "6px",
+            background: "#1F6EB6",
+            border: "1px solid #76A4CE",
+            fontSize: "16px",
+          }}
+        />
+      </div>
     </div>
   );
 };
